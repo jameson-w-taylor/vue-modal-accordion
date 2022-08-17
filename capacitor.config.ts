ee9e0988e8c47d84956182c8d9ecdbc94bb51fc3
Jameson Taylor <jameson@ionic.io>
2022-08-17 15:13:44 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'accordion-modal',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
